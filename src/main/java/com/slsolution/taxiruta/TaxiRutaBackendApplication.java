300a15e1301a1829afdafb876b3441228798c8b0
Charlie Quintero <86026887+cdquinterop@users.noreply.github.com>
2025-09-16 14:52:58 -0500

    proyecto base

---
+++ src/main/java/com/slsolution/taxiruta/TaxiRutaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.slsolution.taxiruta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiRutaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiRutaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slsolution/taxiruta/TaxiRutaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slsolution.taxiruta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiRutaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
